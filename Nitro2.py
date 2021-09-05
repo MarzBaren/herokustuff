b35e358df66253e361e443a4a7ce8a56712e891b
MarzBaren <76433651+MarzBaren@users.noreply.github.com>
2021-09-05 12:32:42 +0200

    Update Nitro2.py

--- Nitro2.py
+++ Nitro2.py
@@ -21,6 +21,7 @@ def checker():
                     'https': "socks4://" + proxy}, headers=header).text
 
                 #print(req)
+                print(str(working) + " - " + str(tested) + " - " + str(len(proxy_list)) + " - " + str(proxy_queue.size()))
 
                 if req.__contains__('<p>The owner of this website (discordapp.com) has banned your IP address'):
                     try:
@@ -31,7 +32,7 @@ def checker():
                     break
                 elif req == '{"message": "Unknown Gift Code", "code": 10038}':
                     tested += 1
-                    print(str(working) + " - " + str(tested) + " - " + str(len(proxy_list)) + " - " + str(proxy_queue.size()))
+                    
                     continue
                 
                 working += 1
