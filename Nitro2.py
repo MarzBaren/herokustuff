aba55eea73d8299a0212e1005047903723c121cf
MarzBaren <76433651+MarzBaren@users.noreply.github.com>
2021-09-05 15:01:39 +0200

    Update Nitro2.py

--- Nitro2.py
+++ Nitro2.py
@@ -25,10 +25,10 @@ def checker():
                         proxy_list.remove(proxy)
                     except: pass
                     break
-                elif req.__contains__('You are being rate limited.'):
+                elif req.__contains__('You are being rate limited.') or req.__contains__('<center><h1>502 Bad Gateway</h1></center>'):
                     break
                 elif req == '{"message": "Unknown Gift Code", "code": 10038}':
-                    print(str(working) + " - " + str(tested) + " - " + str(proxy_queue.qsize()) + " - " + str(len(proxy_list)))
+                    #print(str(working) + " - " + str(tested) + " - " + str(proxy_queue.qsize()) + " - " + str(len(proxy_list)))
                     tested += 1
                     continue
                 
